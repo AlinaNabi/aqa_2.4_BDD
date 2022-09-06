17cc35df90bd5f9530f6e38eed3872160964e086
AlinaNabi <amnabieva@gmail.com>
2022-09-06 21:00:05 +0300

    правка кода1

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
-    private ElementsCollection cardList = $$(".list__item div");
+    private static final ElementsCollection cardList = $$(".list .list__item");
     private static SelenideElement firstCardButton = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button");
     private static SelenideElement secondCardButton = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button");
 
